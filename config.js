d06127dfed040ca1c9add70c567ce2a3d83d5657
skadefro <allan@openrpa.dk>
2023-11-22 09:15:49 +0100

    Update schema, add more proto functions

--- config.js
+++ config.js
@@ -1,79 +1,99 @@
 const fs = require('fs');
 const files = fs.readdirSync('.').filter((file) => file.endsWith('.proto'));
 module.exports = {
-    // ERQU
-    files,
-    // Optional
-    dist: 'apischema.json',
-    // Optional
-    formatServicePath: (path) => {
-      const responses = path.replace(/\./g, '/').replace('/openiap/FlowService/', '/rest/v1/');
-      return responses
+  files,
+  dist: 'apischema.json',
+  formatServicePath: (path) => {
+    const responses = path.replace(/\./g, '/').replace('/openiap/FlowService/', '/rest/v1/');
+    return responses
+  },
+  // Optional, will convert long to string by default
+  long: 'number',
+  customSchema: {
+    // swagger: '2.0',
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "OpenIAP api",
+      description: "OpenAPI for openflow. Allow you to work directly with mongodb and amqp message queues",
+      license: {
+        name: "MPL-2.0",
+        url: "https://github.com/open-rpa/openflow/blob/master/LICENSE"
+      },
+      termsOfService: "https://openiap.io/terms",
+      privacyPolicy: "https://openiap.io/privacy"
     },
-    // Optional, will convert long to string by default
-    long: 'number',
-    // Optional
-    // This will merge and overwrite the result parsed from protobuffer file.
-    // `paths` will merge by path
-    // `components` will merge by component except shcemas
-    // customSchema: {
-    //   swagger: '2.0',
-    //   paths: {
-    //     '/api/test': {
-    //       get: {
-    //         responses: {
-    //           200: {
-    //             schema: {
-    //               $ref: 'GetDataResponse', // Tell me the protobuf message name
-    //             },
-    //           },
-    //         },
-    //         params: [],
-    //       },
-    //     },
-    //   },
-    //   components: {
-    //     securitySchemes: {
-    //       cookieAuth: {
-    //         type: 'apiKey',
-    //         in: 'cookie',
-    //         name: 'token',
-    //       },
-    //     },
-    //   },
-    //   security: [
-    //     {
-    //       cookieAuth: [],
-    //     },
-    //   ],
+    // paths: {
     // },
-    // Optional, you may use this hook to overwrite the original transform result.
-    // transform(type, result, args) {
-    //   switch (type) {
-    //     case 'field': {
-    //       const [field, options] = args;
-    //       console.log('message field:', field);
-    //       console.log('options:', options);
-    //       break;
-    //     }
-    //     case 'message': {
-    //       const [message, options] = args;
-    //       console.log('message:', message);
-    //       console.log('options:', options);
-    //       break;
-    //     }
-    //     case 'enum': {
-    //       const [_enum] = args;
-    //       console.log('enum:', _enum);
-    //       break;
-    //     }
-    //     case 'service': {
-    //       const [service, root, options] = args;
-    //       console.log('service:', service);
-    //       console.log('proto root:', root);
-    //       console.log('options:', options);
-    //     }
-    //   }
-    //   return result;
-    // },
-  };
+
+
+    components: {
+      securitySchemes: {
+        OAuth2: {
+          type: 'oauth2'
+        }
+      }
+    },
+    security: [
+      {
+        OAuth2: [],
+      },
+    ],
+  },
+  transform(type, result, args) {
+    // field | message | enum | service
+    if (type === 'service') {
+      const servicekeys = Object.keys(result);
+      for (let x = 0; x < servicekeys.length; x++) {
+        const path = servicekeys[x];
+        const removeendpoints = [
+          "/rest/v1/SetupStream",
+          "/rest/v1/Signin",
+          "/rest/v1/Download",
+          "/rest/v1/Upload",
+          "/rest/v1/Watch",
+          "/rest/v1/UnWatch",
+          "/rest/v1/RegisterQueue",
+          "/rest/v1/UnRegisterQueue",
+          "/rest/v1/RegisterExchange",
+          
+          
+        ]
+        if (removeendpoints.includes(path)) {
+          delete result[path];
+          continue;
+        }
+        const getpoints = [
+          // "/rest/v1/ListCollections",
+        ]
+
+        // console.log('servicekey'. servicekey);
+        console.log('servicekey', path);
+        const service = result[path];
+        const methodkeys = Object.keys(service);
+        for (let y = 0; y < methodkeys.length; y++) {
+          const methodkey = methodkeys[y];
+          const method = service[methodkey];
+          method.operationId =  method.operationId.replace("openiap.FlowService.", "");
+          method.security = [
+            {
+              OAuth2: [],
+            },
+          ];
+          method["is_consequential"] = false;
+          for(let i = 0; i < method.parameters.length; i++) {
+            const param = method.parameters[i];
+            // console.log(param.in); // body/path/query
+            param.required = true;
+          }
+          if (getpoints.includes(path) && method != "get") {
+            service["get"] = method;
+            delete service[methodkey];
+          }
+
+        }
+      }
+    }
+    return result;
+  }
+};
